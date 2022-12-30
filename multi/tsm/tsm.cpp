ba34275afa60ec0e810d074ce7db1e0b922f8b54
djokovic55 <djokovic64@gmail.com>
2022-12-30 13:41:59 +0100

    TSM seq

--- multi/tsm/tsm.cpp
+++ multi/tsm/tsm.cpp
@@ -2,33 +2,63 @@
 #include <iostream>
 #include <bits/stdc++.h>
 using namespace std;
-#define V 4
+#define V 7
+#define PATH V+1
 #define MAX 1000000
-int tsp(int graph[][V], int s)
+struct city_data {
+
+};
+int tsp(int graph[][V], int s, int* best_path)
 {
+    // save the city path
+    int cities[PATH];
+
     vector<int> vertex;
     for (int i = 0; i < V; i++)
         if (i != s)
             vertex.push_back(i);
     int min_cost = MAX;
+
+    cities[0] = s;
+    cities[PATH - 1] = s;
+
     while(next_permutation(vertex.begin(), vertex.end()))
      {
         int current_cost = 0;
         int j = s;
+        
         for (int i = 0; i < vertex.size(); i++) {
-            current_cost += graph[j][vertex[i]];
-            j = vertex[i];
+                current_cost += graph[j][vertex[i]];
+                j = vertex[i];
+                cities[i+1] = vertex[i];
         }
         current_cost += graph[j][s];
+        if(current_cost < min_cost)
+            for(int i = 0; i < PATH; i++)
+                best_path[i] = cities[i];
+
         min_cost = min(min_cost, current_cost);
 	}
 	return min_cost;
 }
 int main()
 {
-    int graph[][V] = { { 0, 10, 15, 20 },{ 10, 0, 35, 25 },{ 15, 35, 0, 30 },{ 20, 25, 30, 0 } };                      
+    int best_path[PATH];
+    int graph[][V] = { {MAX, 5, 4, 2, 9, 1, 5},
+                       {MAX, MAX, 0, 6, 9, 6, 0},
+                       { 5, 3, MAX, 7, 6, 3, 2},
+                       { 5, 2, 7, MAX, 7, 0, 4},
+                       {MAX, 1, 3, 2, MAX, 6, 2},
+                       { 9, 1, 8, 0, 9, MAX, 2},
+                       { 1, 8, 4, 8, 6, 0, MAX}};                      
     int s = 0;
-    cout << tsp(graph, s) << endl;
+    cout << tsp(graph, s, best_path) << endl;
+    cout <<"Visited cities: ";
+    for(int i = 0; i < PATH; i++)
+        cout<< best_path[i]<<" ";
+    cout<<endl;
+
+
     return 0;
 }
 
